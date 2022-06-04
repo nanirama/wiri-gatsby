4d8958468610a0dd492375008982a931f8871a8f
nanirama <nani.rama1982@gmail.com>
2022-06-04 17:20:19 +0530

    DAsboard changes 11

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,13 +7,3 @@ import "slick-carousel/slick/slick-theme.css"
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>;
 };
-
-const loadableReady = require("@loadable/component").loadableReady
-
-exports.replaceHydrateFunction = () => {
-  return (element, container, callback) => {
-    loadableReady(() => {
-      ReactDOM.render(element, container, callback)
-    })
-  }
-}
